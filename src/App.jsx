b4c947e702ddf449fa69fec9ef39802942021e03
esedova149 <aytenesedova149@gmail.com>
2024-08-07 22:31:14 +0400

    commit4

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
 import Service from './components/Service';
+import PricingPlan from './components/PricingPlan';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <About/>
       <Portfolio/>
       <Service/>
+      <PricingPlan/>
     </div>
   );
 }
